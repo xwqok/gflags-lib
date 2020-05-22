95d673432efb7f6a990c4990e3d5ca13b0b05fa2
xuwenqiang <369296313@qq.com>
2020-05-22 14:52:23 +0800

    up

---
+++ CMakeFiles/gflags_nothreads_static.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xwq/gflags/src/gflags.cc" "/home/xwq/gflags/CMakeFiles/gflags_nothreads_static.dir/src/gflags.cc.o"
+  "/home/xwq/gflags/src/gflags_completions.cc" "/home/xwq/gflags/CMakeFiles/gflags_nothreads_static.dir/src/gflags_completions.cc.o"
+  "/home/xwq/gflags/src/gflags_reporting.cc" "/home/xwq/gflags/CMakeFiles/gflags_nothreads_static.dir/src/gflags_reporting.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  "NO_THREADS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "src"
+  "include/gflags"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
